384f92aec8ff612a139b40a92000174b9708bbd9
Patrick Hallbick <patrick.hallbick@outlook.com>
2025-12-15 16:38:41 -0500

    Add schedule

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 A set of OBS sources of varying flexibility I use for my retro-style stream aesthetic to emulate windows and such.
-Originally these were 95-style but I thought they looked better with the gradient.
+Originally these were 95-style but I thought they looked better with the gradient so now it's closer to 98.
+Also I just kinda mix in bits and bobs now.
 
 You're free to use any part of these for whatever, they're pretty straightforward generally.
---
+++ schedule/schedule.js
@@ -0,0 +1,30 @@
+const slots = [
+    {
+        time: "18:30",
+        day: "Monday",
+        name: "Pokémon Legends Z-A",
+        src: "legendsza.mp4",
+        blurb: "Climbing the ranks and catching new mons in Z-A!"
+    },
+    {
+        time: "18:00",
+        day: "Wednesday",
+        name: "Wizard101",
+        src: "wizard101.mp4",
+        blurb: "Revisiting a classic with improved performance and saving the magical world in Wizard101!"
+    },
+    {
+        time: "13:00",
+        day: "Friday",
+        name: "Murder Aboard the Harpy Express",
+        src: "minecraft.mp4",
+        blurb: "Whodunnit??? Join blur, Astra, and the rest of the crew aboard the Harpy Express in this Minecraft-based adventure!"
+    },
+    {
+        time: "18:00",
+        day: "Saturday",
+        name: "Bug Fables+",
+        src: "bfplus.mp4",
+        blurb: "We're making our way out west in Bug Fables+ to find a way to the Deadlands."
+    },
+];
